52d496c3366774a3f92cf86720888b9cff6a7c6e
Arun Lab <121912626+Labacct123@users.noreply.github.com>
2023-01-13 00:34:21 -0600

    Update streamlit_app.py
    Added code block to display the fruit list from table

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,18 +47,22 @@ try:
 except URLError as e:
   streamlit.error()
 
-#don't run anything past this point while we troubleshoot
-streamlit.stop()
-
-
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake related function
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("SELECT * from fruit_load_list")
+         return my_cur.fetchall()
+    
+#Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()    
+    streamlit.dataframe(my_data_rows)
 
+#Don't anything past this point
+streamlit.stop()
+    
 #New section to get and display Fruit Choice
 streamlit.header("User Fruit Choice")
 fruit_choice = streamlit.text_input('Enter the user fruit choice','Mango')
